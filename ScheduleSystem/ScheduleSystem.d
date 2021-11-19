24eb00a24cc63925e516e4b156ac7a4b83a331dc
RockFall <caiohdrocha@gmail.com>
2021-11-18 21:02:31 -0300

    Finnaly setup working

--- ScheduleSystem/ScheduleSystem.d
+++ ScheduleSystem/ScheduleSystem.d
@@ -1,3 +1,3 @@
 obj/ScheduleSystem.o: \
- /home/rockfall/projects/matching--scheduling-system/ScheduleSystem/ScheduleSystem.cpp \
- /home/rockfall/projects/matching--scheduling-system/ScheduleSystem/ScheduleSystem.h
+ /home/rockfall/projects/matching--scheduling-system/ScheduleSystem/src/ScheduleSystem.cpp \
+ /home/rockfall/projects/matching--scheduling-system/ScheduleSystem/src/ScheduleSystem.h
--- ScheduleSystem/main.d
+++ ScheduleSystem/main.d
@@ -1,3 +1,3 @@
 obj/main.o: \
- /home/rockfall/projects/matching--scheduling-system/ScheduleSystem/main.cpp \
- /home/rockfall/projects/matching--scheduling-system/ScheduleSystem/ScheduleSystem.h
+ /home/rockfall/projects/matching--scheduling-system/ScheduleSystem/src/main.cpp \
+ /home/rockfall/projects/matching--scheduling-system/ScheduleSystem/src/ScheduleSystem.h
