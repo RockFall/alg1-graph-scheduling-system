e8fc9e336b56a7c135c4463e5be0ec22532cb9cf
RockFall <caiohdrocha@gmail.com>
2021-11-20 00:48:57 -0300

    Grid e scheduler

--- ScheduleSystem/src/Grid.h
+++ ScheduleSystem/src/Grid.h
@@ -2,15 +2,25 @@
 
 #include "Vector2.h"
 
+#include <vector>
+
 class Grid
 {
 public:
   Grid(int w, int h);
   ~Grid() { };
 
+  int dijkstra(const std::vector<std::vector<int>> &graph, Vector2 source, Vector2 target);
 
+
+  void CreateGraph();
+  std::vector<int> GetAllDistancesFromVector();
 
 private:
+  // Graph of a grid -> A vector representing each tile and holds a vector of it's neighbours
+  std::vector<std::vector<int>> gridGraph_;
   int width_;
   int height_;
+
+  int getId(Vector2 v);
 };
---
+++ ScheduleSystem/src/Scheduler.cpp
@@ -0,0 +1,8 @@
+#include "Scheduler.h"
+
+#include <vector>
+
+void Scheduler::Schedule()
+{
+  std::vector<int, std::vector<int>> proximity_list;
+}
